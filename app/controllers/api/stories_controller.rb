e851eec3a32a5915d20d8b312edbce2059079c4a
K-Ramberg <kramberg0350@gmail.com>
2018-07-25 15:18:51 -0400

    story show setup

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -3,4 +3,10 @@ class Api::StoriesController < ApplicationController
         @stories = Story.all
         render json: @stories
     end
+
+    def show
+        @story = Story.find(params[:id])
+        render json: @story
+    end
+
 end
--- client/src/components/story_components/StoryShow.js
+++ client/src/components/story_components/StoryShow.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class StoryShow extends Component {
+
+    state = {
+        story: {}
+    }
+
+    componentDidMount() {
+        this.fetchStory()
+    }
+
+    fetchStory = async () => {
+        try{
+            let storyResponse = await axios.get(`/api/users/${this.props.match.params.user_id}/stories/${this.props.match.params.id}`)
+            this.setState({
+                story: storyResponse.data
+            })
+        } catch(err){
+            console.error(err)
+        }
+    }
+
   render() {
     return (
       <div>
-        the story will be here
+        <h1>{this.state.story.title}</h1>
+        <h3>theme select {this.state.story.theme}</h3>
+        <h3>difficulty select {this.state.story.difficulty}</h3>
       </div>
     )
   }
