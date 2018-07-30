9819f92eb240a5130aa94d31565fe5fde13e3076
K-Ramberg <kramberg0350@gmail.com>
2018-07-30 12:20:06 -0400

    adding story fail function

--- client/src/components/story_components/StoryOverFail.js
+++ client/src/components/story_components/StoryOverFail.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default class StoryOverFail extends Component {
+
+  handleEndOfStoryFail = async () => {
+    await axios.delete(`/api/users/${this.props.match.params.user_id}/stories/${this.props.location.state.story}`)
+    await this.props.history.push(`/users/${this.props.match.params.user_id}/stories`)
+  }
+
   render() {
     return (
       <div>
-        <h1>Out of chances Sorry</h1>
-        <Link to={`/users/${this.props.match.params.user_id}/stories`}>back to stories</Link>
+        <h1>Sorry Out of Chances</h1>
+        <button onClick={this.handleEndOfStoryFail}>back to stories</button>
       </div>
     )
   }
