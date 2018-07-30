4ffd1a521e4d828505929af30ce5c40875a8d92b
K-Ramberg <kramberg0350@gmail.com>
2018-07-30 19:41:23 -0400

    adding carousel to form component

--- client/src/components/character_components/CharacterForm.js
+++ client/src/components/character_components/CharacterForm.js
@@ -4,8 +4,13 @@ import styled from 'styled-components'
 
 const FormWrapper = styled.div`
     line-height: 3vh;
+    max-height: 5vh;
+    text-align: center;
     .carousel-inner {
         height: 300px;
+    }
+    .carousel-control{
+        max-height: 5vh;
     }
 `
 
@@ -13,90 +18,139 @@ export default class CharacterForm extends Component {
 
     state = {
         index: 0,
-        direction: null
+        direction: null,
+        index2: 0,
+        direction2: null,
+        index3: 0,
+        direction3: null,
+        index4: 0,
+        direction4: null
     };
 
-handleSelect = (selectedIndex, e) => {
-    this.setState({
-        index: selectedIndex,
-        direction: e.direction
-    });
-}
-render() {
+    handleSelect = (selectedIndex, e) => {
+        this.setState({
+            index: selectedIndex,
+            direction: e.direction
+        });
+    }
 
-    const { index, direction } = this.state;
+    handleSelect2 = (selectedIndex, e) => {
+        this.setState({
+            index2: selectedIndex,
+            direction2: e.direction
+        });
+    }
 
-    return (
-        <div>
-            <form onSubmit={this.props.submit}>
-                <div>
-                    <label htmlFor="name">Call me...</label>
-                    <input type="text" name="name" value={this.props.character.name} placeholder="my name is" onChange={this.props.formChange} />
-                </div>
-                <div id="occupation">
-                    <label htmlFor="occupation">I am a...</label>
-                    <input type="radio" name="occupation" value="Princess" onChange={this.props.formChange} checked={this.props.character.occupation === 'Princess'} />Princess
+    handleSelect3 = (selectedIndex, e) => {
+        this.setState({
+            index3: selectedIndex,
+            direction3: e.direction
+        });
+    }
+
+    handleSelect4 = (selectedIndex, e) => {
+        this.setState({
+            index4: selectedIndex,
+            direction4: e.direction
+        });
+    }
+
+    render() {
+
+        const { index, index2, index3, index4, direction, direction2, direction3, direction4 } = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.props.submit}>
+                    <div>
+                        <label htmlFor="name">Call me...</label>
+                        <input type="text" name="name" value={this.props.character.name} placeholder="my name is" onChange={this.props.formChange} />
+                    </div>
+                    <div id="occupation">
+                        <label htmlFor="occupation">I am a...</label>
+                        <input type="radio" name="occupation" value="Princess" onChange={this.props.formChange} checked={this.props.character.occupation === 'Princess'} />Princess
                     <input type="radio" name="occupation" value="Wizard" onChange={this.props.formChange} checked={this.props.character.occupation === 'Wizard'} />Wizard
                     <input type="radio" name="occupation" value="Dinosaur" onChange={this.props.formChange} checked={this.props.character.occupation === 'Dinosaur'} />Dinosaur
                 </div>
-                <div>
-                    <label htmlFor="head_element">Head:</label>
-                    <input type="radio" name="head_element" value="1" onChange={this.props.formChange} checked={this.props.character.head_element == 1} /> 1
-                    <input type="radio" name="head_element" value="2" onChange={this.props.formChange} checked={this.props.character.head_element == 2} /> 2
-                    <input type="radio" name="head_element" value="3" onChange={this.props.formChange} checked={this.props.character.head_element == 3} /> 3
-                </div>
-                <div>
-                    <label htmlFor="body_element">Body:</label>
-                    <input type="radio" name="body_element" value="1" onChange={this.props.formChange} checked={this.props.character.body_element == 1} /> 1
-                    <input type="radio" name="body_element" value="2" onChange={this.props.formChange} checked={this.props.character.body_element == 2} /> 2
-                    <input type="radio" name="body_element" value="3" onChange={this.props.formChange} checked={this.props.character.body_element == 3} /> 3
-                </div>
-                <div>
-                    <label htmlFor="leg_element">Legs:</label>
-                    <input type="radio" name="leg_element" value="1" onChange={this.props.formChange} checked={this.props.character.leg_element == 1} /> 1
-                    <input type="radio" name="leg_element" value="2" onChange={this.props.formChange} checked={this.props.character.leg_element == 2} /> 2
-                    <input type="radio" name="leg_element" value="3" onChange={this.props.formChange} checked={this.props.character.leg_element == 3} /> 3
-                </div>
-                <div>
-                    <label htmlFor="color_scheme">Color</label>
-                    <input type="radio" name="color_scheme" value="1" onChange={this.props.formChange} checked={this.props.character.color_scheme == 1} /> 1
-                    <input type="radio" name="color_scheme" value="2" onChange={this.props.formChange} checked={this.props.character.color_scheme == 2} /> 2
-                    <input type="radio" name="color_scheme" value="3" onChange={this.props.formChange} checked={this.props.character.color_scheme == 3} /> 3
-                </div>
-                <button type="submit">Let's Go!</button>
-            </form>
-            <FormWrapper>
-                <Carousel
-                    activeIndex={index}
-                    direction={direction}
-                    onSelect={this.handleSelect}
-                >
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+                    <div>
+                    <FormWrapper>
+                    <Carousel htmlFor="head_element"
+                        activeIndex={index}
+                        direction={direction}
+                        onSelect={this.handleSelect}
+                         >
+                        <Carousel.Item >
+                            <input type="radio" name="head_element" value="1" onChange={this.props.formChange} checked={this.props.character.head_element == 1} /> 1
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <input type="radio" name="head_element" value="2" onChange={this.props.formChange} checked={this.props.character.head_element == 2} /> 2
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <input type="radio" name="head_element" value="3" onChange={this.props.formChange} checked={this.props.character.head_element == 3} /> 3
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+                    </Carousel>
+                </FormWrapper>
+                </div>
+                    <div>
+                    <FormWrapper>
+                    <Carousel htmlFor="body_element"
+                        activeIndex={index2}
+                        direction={direction2}
+                        onSelect={this.handleSelect2}
+                         >
+                        <Carousel.Item >
+                            <input type="radio" name="body_element" value="1" onChange={this.props.formChange} checked={this.props.character.body_element == 1} /> 1
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <input type="radio" name="body_element" value="2" onChange={this.props.formChange} checked={this.props.character.body_element == 2} /> 2
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <input type="radio" name="body_element" value="3" onChange={this.props.formChange} checked={this.props.character.body_element == 3} /> 3
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-                        </Carousel.Caption>
+                    </Carousel>
+                </FormWrapper>
+                </div>
+                    <div>
+                    <FormWrapper>
+                    <Carousel htmlFor="leg_element"
+                        activeIndex={index3}
+                        direction={direction3}
+                        onSelect={this.handleSelect3}
+                         >
+                        <Carousel.Item >
+                                <input type="radio" name="leg_element" value="1" onChange={this.props.formChange} checked={this.props.character.leg_element == 1} /> 1
+                        </Carousel.Item>
+                        <Carousel.Item>
+                                <input type="radio" name="leg_element" value="2" onChange={this.props.formChange} checked={this.props.character.leg_element == 2} /> 2
+                        </Carousel.Item>
+                        <Carousel.Item>
+                                <input type="radio" name="leg_element" value="3" onChange={this.props.formChange} checked={this.props.character.leg_element == 3} /> 3
                     </Carousel.Item>
-                </Carousel>
-            </FormWrapper>
-        </div>
-    )
-}
+                    </Carousel>
+                </FormWrapper>
+                </div>
+                    <div>
+                    <FormWrapper>
+                    <Carousel htmlFor="color_scheme"
+                        activeIndex={index4}
+                        direction={direction4}
+                        onSelect={this.handleSelect4}
+                         >
+                        <Carousel.Item >
+                                <input type="radio" name="color_scheme" value="1" onChange={this.props.formChange} checked={this.props.character.color_scheme == 1} /> 1
+                        </Carousel.Item>
+                        <Carousel.Item>
+                                <input type="radio" name="color_scheme" value="2" onChange={this.props.formChange} checked={this.props.character.color_scheme == 2} /> 2
+                        </Carousel.Item>
+                        <Carousel.Item>
+                                <input type="radio" name="color_scheme" value="3" onChange={this.props.formChange} checked={this.props.character.color_scheme == 3} /> 3
+                    </Carousel.Item>
+                    </Carousel>
+                </FormWrapper>
+                </div>
+                    <button type="submit">Let's Go!</button>
+                </form>
+            </div>
+        )
+    }
 }
