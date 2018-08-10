6cd046c8d67d2e6a98c8adee8b01a808fc5909d7
K-Ramberg <kramberg0350@gmail.com>
2018-08-10 17:33:57 -0400

    added glasses component

---
+++ client/src/components/konva_shapes/sub_char_shapes/Glasses.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Konva from "konva";
+import { Shape, Group } from "react-konva";
+
+ export default class Glasses extends React.Component {
+    state = {
+      color: Konva.Util.getRandomColor()
+    };
+    handleClick = () => {
+      this.setState({
+        color: Konva.Util.getRandomColor()
+      });
+    };
+    render() {
+      return (
+        <Group
+          x={220}
+          y={20}
+        >
+        <Shape
+              sceneFunc={(context, shape) => {
+                context.beginPath();
+                context.moveTo(15, 38);
+                context.quadraticCurveTo(30, 58, 35, 40)
+                context.lineTo(15, 38)
+                context.fillStrokeShape(shape);
+              }}
+              stroke="red"
+              strokeWidth={2}
+              shadowBlur={2}
+              fill="#BEF4EF"
+            />
+            <Shape
+              sceneFunc={(context, shape) => {
+                context.beginPath();
+                context.moveTo(38, 40);
+                context.quadraticCurveTo(45, 58, 57, 38)
+                context.lineTo(38, 40)
+                context.fillStrokeShape(shape);
+              }}
+              stroke="red"
+              strokeWidth={2}
+              shadowBlur={2}
+              fill="#BEF4EF"
+            />
+        </Group>
+      );
+    }
+  }
